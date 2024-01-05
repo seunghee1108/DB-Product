f43fb1d00329abae41c0ad71c346550c44cf6089
승희 <bangseunghee1108@gmail.com>
2024-01-05 18:08:47 +0900

    서버 파일 코드 작성

---
+++ app.js
@@ -0,0 +1,42 @@
+import http from "http";
+import fs from "fs";
+
+const PORT = 3000;
+
+const fetchHtmlPath = "./index.html"
+const xhrHtmlPath = "./xhr.html"
+
+const connection = mysql.createServer({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "",
+});
+
+const server = http.createServer((req, res) => {
+  if (res.method === "GET" && req.url === "/") {
+    fs.readFile(fetchHtmlPath, 'utf8', (err, data) => {
+      fs.writeHead(200, { "Content-Type" : "text/html" });
+    });
+  }
+  else if (req.method === "GET" && req.url === "/mariaDB") {
+    const query = "SELECT * FROM product";
+
+    connection.query(query, (error, result) => {
+      if (error) {
+        res.writeHead(500, { "Content-Type" : "application/json" });
+        res.end(JSON.stringify({ error : "Internal Server Error" }));
+      }
+      else {
+        res.writeHead(200, { "Content-Type" : "application/json" });
+        res.end(JSON.stringify(result));
+      }
+    })
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`
+Server running at http://localhost:${PORT}
+  `);
+});
