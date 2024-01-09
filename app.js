5ae55027f783e2101f7cff7b618afcd374a43637
bangseunghee <bangseunghee1108@gmail.com>
2024-01-09 11:50:01 +0900

    서버코드 수정

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
 import http from "http";
 import fs from "fs";
+import mysql from "mysql";
 
 const PORT = 3000;
 
 const fetchHtmlPath = "./index.html"
 // const xhrHtmlPath = "./xhr.html"
 
-const connection = mysql.createServer({
+
+const connection = mysql.createConnection({  // createServer가 아닌 createConnection을 사용해야 합니다.
   host: "localhost",
   user: "root",
   password: "1108",
-  database: "product",  // db 이름
+  database: "dbproduct",
 });
 
 const server = http.createServer((req, res) => {
-  if (res.method === "GET" && req.url === "/") {
+  if (req.method === "GET" && req.url === "/") {  // res.method 대신 req.method을 사용해야 합니다.
     fs.readFile(fetchHtmlPath, 'utf8', (err, data) => {
-      fs.writeHead(200, { "Content-Type" : "text/html" });
+      if (err) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Internal Server Error" }));
+        return;
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
     });
-  }
-  else if (req.method === "GET" && req.url === "/mariaDB") {
+  } else if (req.method === "GET" && req.url === "/product") {
     const query = "SELECT * FROM product";
 
     connection.query(query, (error, result) => {
       if (error) {
-        res.writeHead(500, { "Content-Type" : "application/json" });
-        res.end(JSON.stringify({ error : "Internal Server Error" }));
-      }
-      else {
-        res.writeHead(200, { "Content-Type" : "application/json" });
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Internal Server Error" }));
+      } else {
+        res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify(result));
       }
-    })
+    });
   }
-})
+});
 
 server.listen(PORT, () => {
   console.log(`
